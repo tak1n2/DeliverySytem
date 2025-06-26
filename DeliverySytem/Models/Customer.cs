fe3fa2081e1b87fb903d33162e3ffeec5fe062f0
Yarem_ql39@student.itstep.org <Yarem_ql39@student.itstep.org>
2025-06-26 21:49:37 +0300

    Add model Customer and controller and view

---
+++ DeliverySytem/Models/Customer.cs
@@ -0,0 +1,55 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace DeliverySytem.Models
+{
+    public class Customer : ITableEntity
+    {
+       
+
+        public string Name { get; set; }
+        public string CityKey { get; set; }
+        public string CountryKey { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int TotalOrdersSum { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        public Customer(string name, string cityKey, string countryKey, string email, string password)
+        {
+            Name = name;
+            CityKey = cityKey;
+            CountryKey = countryKey;
+            Email = email;
+            Password = password;
+            TotalOrdersSum = 0;
+            CreatedAt = DateTime.UtcNow;
+
+            PartitionKey = "Customer";
+            RowKey = Guid.NewGuid().ToString();
+            Timestamp = DateTimeOffset.UtcNow;
+            ETag = new ETag();
+        }
+
+        public Customer()
+        {
+            Name = "";
+            CityKey = "";
+            CountryKey = "";
+            Email = "";
+            Password = "";
+            TotalOrdersSum = 0;
+            CreatedAt = DateTime.UtcNow;
+
+            PartitionKey = "Customer";
+            RowKey = Guid.NewGuid().ToString();
+            Timestamp = DateTimeOffset.UtcNow;
+            ETag = new ETag();
+        }
+    }
+}
