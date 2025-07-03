ee6b1636cb062dedb69134b0bf7d308a27ce40c7
hitman4ik85 <hitman4ik85@gmail.com>
2025-07-03 21:52:25 +0300

    Upd MainConteiner, Index, model Order OrderLine, Buy

--- DeliverySytem/Controllers/MainController.cs
+++ DeliverySytem/Controllers/MainController.cs
@@ -1,10 +1,18 @@
-﻿using DeliverySytem.Models;
+﻿using Azure.Data.Tables;
+using DeliverySytem.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeliverySytem.Controllers
 {
     public class MainController : Controller
     {
+        private readonly TableClient tableClient;
+
+        public MainController(TableClient tableClient)
+        {
+            this.tableClient = tableClient;
+        }
+
         public IActionResult Index()
         {
             List<Product> products = new List<Product>
@@ -13,7 +21,27 @@ namespace DeliverySytem.Controllers
                 new Product("Smartphone", "Latest model smartphone", 800, "https://example.com/smartphone.jpg", "shop2"),
                 new Product("Headphones", "Noise-cancelling headphones", 200, "https://example.com/headphones.jpg", "shop3"),
             };
+
             return View(products);
+        }
+
+        public IActionResult Buy(string productName, int price, string shopKey)
+        {
+            string? loggedId = HttpContext.Session.GetString("LoggedId");
+            if (string.IsNullOrEmpty(loggedId))
+            {
+                return RedirectToAction("Login", "Register");
+            }
+
+            Order order = new Order(loggedId, price);
+            tableClient.AddEntity(order);
+
+            OrderLine orderLine = new OrderLine(order.RowKey, productName, price, shopKey);
+            tableClient.AddEntity(orderLine);
+
+            ViewBag.SuccessMessage = $"Order created for {productName}.";
+
+            return RedirectToAction("Index");
         }
     }
 }
