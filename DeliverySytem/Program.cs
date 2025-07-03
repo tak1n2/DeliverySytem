86d9d02e2ecf44d3994c0348d8dacf61ff41ecc9
Demydov Maksym <maksimdemidov05@gmail.com>
2025-07-03 20:06:05 +0300

    connection string template

--- DeliverySytem/Program.cs
+++ DeliverySytem/Program.cs
@@ -1,3 +1,6 @@
+using Azure.Data.Tables;
+using Azure.Storage.Blobs;
+
 namespace DeliverySytem
 {
     public class Program
@@ -6,8 +9,39 @@ namespace DeliverySytem
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            string connectionString = "";
+			string blobContainer = "shop";
+            string tableName = "shop";
+
+			//connect to Blob storage
+			builder.Services.AddSingleton(options => {
+				BlobServiceClient serviceClient = new BlobServiceClient(connectionString);
+				BlobContainerClient containerClient = serviceClient.GetBlobContainerClient(blobContainer);
+				containerClient.CreateIfNotExists();
+				return containerClient;
+			});
+
+			//connect to Table storage
+			builder.Services.AddSingleton(options => {
+				TableServiceClient serviceClient = new TableServiceClient(connectionString);
+				TableClient tableClient = serviceClient.GetTableClient(tableName);
+				tableClient.CreateIfNotExists();
+				return tableClient;
+			});
+
+			//Session to use only in code
+			builder.Services.AddSession(options => {
+				options.IdleTimeout = TimeSpan.FromMinutes(20);
+				options.Cookie.HttpOnly = true;
+				options.Cookie.IsEssential = true;
+			});
+
+			//Session to use on views
+			builder.Services.AddHttpContextAccessor();
+
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
@@ -20,11 +54,13 @@ namespace DeliverySytem
 
             app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseSession();
+
+			app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Register}/{action=Login}/{id?}");
 
             app.Run();
         }
